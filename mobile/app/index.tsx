4d0e1abdf0bbbe462924cf24b08e8c46f8abf193
pravin <pravin709266@gmail.com>
2026-02-05 14:26:12 +0530

    react native(Expo) basic setup completed

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl text-green">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
